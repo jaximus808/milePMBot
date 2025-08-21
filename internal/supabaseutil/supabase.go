93a758a96657e55576ceb73a1e9fdaf51e363ab1
observer <j.k.poentis@wustl.edu>
2025-08-20 15:06:32 -1000

    feat: fixing keys

--- internal/supabaseutil/supabase.go
+++ internal/supabaseutil/supabase.go
@@ -11,7 +11,7 @@ var Client *supabase.Client
 
 func InitializeSupabase() error {
 
-	client, err := supabase.NewClient(os.Getenv("APP_SUPABASE_URL"), os.Getenv("APP_SUPABASE_ANON_KEY"), &supabase.ClientOptions{})
+	client, err := supabase.NewClient(os.Getenv("APP_SUPABASE_URL"), os.Getenv("APP_SUPABASE_SERVICE_KEY"), &supabase.ClientOptions{})
 	if err != nil {
 		return fmt.Errorf("failed to open supabase client: %w", err)
 	}
